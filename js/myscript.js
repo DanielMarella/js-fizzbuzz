f8094efa166dbc8498dff7b774b79ec366b48f98
DanielMarella <marella.daniel96@gmail.com>
2023-05-16 16:01:54 +0200

    add visualization of FizzBuzz in console for multiply number by 3 and 5

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,16 @@
 
 
 for (let number = 0; number <= 100; number++){
-    console.log(number);
-    if (number % 3 == 0 ){
+    if(number % 15 === 0){
+        console.log('FizzBuzz');
+    }
+    else if (number % 3 == 0 ){
         console.log('Fizz');
     }
     else if(number % 5 === 0){
         console.log('Buzz');
+    }
+    else{
+        console.log(number);
     }
 }
