67379c28c16472a9606fbca926c5041688403eb9
DanielMarella <marella.daniel96@gmail.com>
2023-05-16 15:38:47 +0200

    fix the code on js for the number divisible by 3, add  the division by  5

--- js/myscript.js
+++ js/myscript.js
@@ -2,5 +2,10 @@
 
 for (let number = 0; number <= 100; number++){
     console.log(number);
+    if (number % 3 == 0 ){
+        console.log('Fizz');
+    }
+    else if(number % 5 === 0){
+        console.log('Buzz');
+    }
 }
-
